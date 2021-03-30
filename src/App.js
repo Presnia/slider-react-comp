e5f0c7458ca7cf264c83f826380da0a06c365d51
Andrei Dzebysh <presnia@yahoo.com>
2021-03-30 15:15:08 +0300

    A new SliderPage component has been created

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
 import React from "react";
 import {hot} from "react-hot-loader";
-import Card from "./components/Card/Card.js";
-import alone from '../src/assets/img/alone.jpeg';
-import camomile from '../src/assets/img/camomile.jpg';
-import mountain from '../src/assets/img/mountain.jpeg';
-import rock from '../src/assets/img/rock.jpeg';
-import waterfall from '../src/assets/img/waterfall.jpeg';
-
+import SliderPage from "./components/SliderPage/SliderPage";
 import "./App.css";
 
 const App = () => {
 
-  const images = [
-    alone,
-    camomile,
-    mountain,
-    rock,
-    waterfall
-  ];
-
     return(
       <div className="slider">
         <h1>Slider</h1>
-        <section className="slider-wrapper">
-          {
-            images.map(pics => <Card img={pics}/>)
-          }
-        </section>
+        <SliderPage />
       </div>
     );
 }
